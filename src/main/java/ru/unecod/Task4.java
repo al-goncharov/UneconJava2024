3b7c38906ca17e2c6c44cff26f3cf9ae1fa35674
al-goncharov <switchemail@yandex.ru>
2024-10-14 17:05:51 +0300

    Implement phone number validation and formatting function
    - Created `phoneNumbers` method to process a list of Russian phone numbers.
    - Validated phone numbers to ensure they contain 7 or 10 digits.
    - Removed prefixes `+7` and `8`, and eliminated non-digit characters.
    - Returned a list of standardized phone numbers without spaces or hyphens.
    - Added unit tests to verify functionality and correctness of the implementation.
    - Organized the project as a Maven application.

---
+++ src/main/java/ru/unecod/Task4.java
@@ -0,0 +1,17 @@
+package ru.unecod;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Task4 {
+    public static List<String> phoneNumbers(List<String> list) {
+
+        Pattern numberPattern = Pattern.compile("(?:\\+7|8)?(\\(\\d{3}\\))?\\d{3}-?\\d{2}-?\\d{2}");
+
+        return list.stream()
+                .filter(number -> numberPattern.matcher(number).matches())
+                .map(number -> number.replaceAll("^(8|\\+7)", "").replaceAll("\\D", ""))
+                .filter(number -> number.length() == 7 || number.length() == 10)
+                .toList();
+    }
+}
---
+++ src/test/java/ru/unecod/Task4Test.java
@@ -0,0 +1,37 @@
+package ru.unecod;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
+public class Task4Test {
+
+    @Test
+    public void validTenDigitNumberWithPrefix() {
+        List<String> input = List.of("+7(921)924-3276", "8(911)111-22-33");
+        List<String> expected = List.of("9219243276", "9111112233");
+        assertEquals(expected, Task4.phoneNumbers(input));
+    }
+
+    @Test
+    public void validTenDigitNumberWithoutPrefix() {
+        List<String> input = List.of("(921)924-3276", "(911)1112233");
+        List<String> expected = List.of("9219243276", "9111112233");
+        assertEquals(expected, Task4.phoneNumbers(input));
+    }
+
+    @Test
+    public void validSevenDigitNumber() {
+        List<String> input = List.of("1234567", "123-4567", "765-43-21");
+        List<String> expected = List.of("1234567", "1234567", "7654321");
+        assertEquals(expected, Task4.phoneNumbers(input));
+    }
+
+    @Test
+    public void invalidNumbers() {
+        List<String> input = List.of("+7 921 924 32 76", "2222-222", "8911111-22-33", "12-345-67", "9279243276");
+        List<String> expected = List.of();
+        assertEquals(expected, Task4.phoneNumbers(input));
+    }
+}
+
